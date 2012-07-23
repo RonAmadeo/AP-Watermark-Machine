2d240e323846902356494935569715630c07fe09
Ron Amadeo <ronamadeo@gmail.com>
2012-07-23 16:13:41 -0400

    Inital Push

---
+++ WindowsApplication1/WindowsApplication1/APWatermark.Designer.vb
@@ -0,0 +1,257 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TWM_Form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(TWM_Form))
+        Me.txt_input = New System.Windows.Forms.TextBox()
+        Me.folderPath = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.btn_startwatch = New System.Windows.Forms.Button()
+        Me.btn_stop = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txt_output = New System.Windows.Forms.TextBox()
+        Me.TrayIcon = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ShowToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txt_input
+        '
+        Me.txt_input.AllowDrop = True
+        Me.txt_input.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.txt_input.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystemDirectories
+        Me.txt_input.Location = New System.Drawing.Point(13, 171)
+        Me.txt_input.Name = "txt_input"
+        Me.txt_input.Size = New System.Drawing.Size(190, 20)
+        Me.txt_input.TabIndex = 1
+        '
+        'folderPath
+        '
+        Me.folderPath.Location = New System.Drawing.Point(209, 169)
+        Me.folderPath.Name = "folderPath"
+        Me.folderPath.Size = New System.Drawing.Size(75, 23)
+        Me.folderPath.TabIndex = 2
+        Me.folderPath.Text = "Browse"
+        Me.folderPath.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(10, 155)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(156, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Automatically watermark files in:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(42, 12)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(123, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "We're going to stick this:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.InitialImage = Nothing
+        Me.PictureBox1.Location = New System.Drawing.Point(45, 28)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(204, 92)
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(89, 123)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(163, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "on EVERYTHING. Automatically."
+        '
+        'FolderBrowserDialog1
+        '
+        Me.FolderBrowserDialog1.Description = "Pick a folder for automatic watermarking:"
+        '
+        'btn_startwatch
+        '
+        Me.btn_startwatch.Location = New System.Drawing.Point(50, 270)
+        Me.btn_startwatch.Name = "btn_startwatch"
+        Me.btn_startwatch.Size = New System.Drawing.Size(94, 23)
+        Me.btn_startwatch.TabIndex = 3
+        Me.btn_startwatch.Text = "Start Watching"
+        Me.btn_startwatch.UseVisualStyleBackColor = True
+        '
+        'btn_stop
+        '
+        Me.btn_stop.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btn_stop.Enabled = False
+        Me.btn_stop.Location = New System.Drawing.Point(150, 270)
+        Me.btn_stop.Name = "btn_stop"
+        Me.btn_stop.Size = New System.Drawing.Size(94, 23)
+        Me.btn_stop.TabIndex = 4
+        Me.btn_stop.Text = "Stop Watching"
+        Me.btn_stop.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(10, 198)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(139, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Output watermarked files to:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(209, 212)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txt_output
+        '
+        Me.txt_output.AllowDrop = True
+        Me.txt_output.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.txt_output.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystemDirectories
+        Me.txt_output.Location = New System.Drawing.Point(13, 212)
+        Me.txt_output.Name = "txt_output"
+        Me.txt_output.Size = New System.Drawing.Size(190, 20)
+        Me.txt_output.TabIndex = 8
+        '
+        'TrayIcon
+        '
+        Me.TrayIcon.BalloonTipText = "Automatic watermarking started! " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "I'll be down here if you need me. <3"
+        Me.TrayIcon.BalloonTipTitle = "The Watermark Machine"
+        Me.TrayIcon.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.TrayIcon.Icon = CType(resources.GetObject("TrayIcon.Icon"), System.Drawing.Icon)
+        Me.TrayIcon.Text = "The Watermark Machine"
+        Me.TrayIcon.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ShowToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(132, 48)
+        Me.ContextMenuStrip1.Text = "ContextMenu"
+        '
+        'ShowToolStripMenuItem
+        '
+        Me.ShowToolStripMenuItem.Name = "ShowToolStripMenuItem"
+        Me.ShowToolStripMenuItem.Size = New System.Drawing.Size(131, 22)
+        Me.ShowToolStripMenuItem.Text = "Show/Hide"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(131, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.Label5.Location = New System.Drawing.Point(234, 294)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 12)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Version 1.0b9"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 241)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(208, 17)
+        Me.CheckBox1.TabIndex = 13
+        Me.CheckBox1.Text = "Remember this and don't ask next time"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TWM_Form
+        '
+        Me.AcceptButton = Me.btn_startwatch
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btn_stop
+        Me.ClientSize = New System.Drawing.Size(295, 305)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txt_output)
+        Me.Controls.Add(Me.btn_stop)
+        Me.Controls.Add(Me.btn_startwatch)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.folderPath)
+        Me.Controls.Add(Me.txt_input)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "TWM_Form"
+        Me.Text = "The Watermark Machine"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txt_input As System.Windows.Forms.TextBox
+    Friend WithEvents folderPath As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents btn_startwatch As System.Windows.Forms.Button
+    Friend WithEvents btn_stop As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txt_output As System.Windows.Forms.TextBox
+    Friend WithEvents TrayIcon As System.Windows.Forms.NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents ShowToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+
+End Class
---
+++ WindowsApplication1/WindowsApplication1/APWatermark.vb
@@ -0,0 +1,214 @@
+﻿Imports System.IO
+Imports System.Diagnostics
+Public Class TWM_Form
+    Public watchfolder As FileSystemWatcher
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        FolderBrowserDialog1.ShowDialog()
+        txt_input.Text = FolderBrowserDialog1.SelectedPath
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_stop.Click
+        ' Stop watching the folder
+        watchfolder.EnableRaisingEvents = False
+        btn_startwatch.Enabled = True
+        btn_stop.Enabled = False
+
+    End Sub
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_startwatch.Click
+        If txt_input.Text = "" Then
+            MessageBox.Show("You need to pick a folder.", "Hey Dummy!")
+        Else
+            If Not Directory.Exists(txt_input.Text) Then
+                MessageBox.Show("That isn't a folder.", "Hey Dummy!")
+            Else
+                If txt_output.Text = "" Then txt_output.Text = txt_input.Text
+
+                If Not Directory.Exists(txt_output.Text) Then
+                    Directory.CreateDirectory(txt_output.Text)
+                End If
+
+                watchfolder = New System.IO.FileSystemWatcher()
+
+                watchfolder.Path = txt_input.Text
+
+                watchfolder.NotifyFilter = IO.NotifyFilters.DirectoryName
+                watchfolder.NotifyFilter = watchfolder.NotifyFilter Or _
+                                           IO.NotifyFilters.FileName
+                watchfolder.NotifyFilter = watchfolder.NotifyFilter Or _
+                                           IO.NotifyFilters.Attributes
+
+                AddHandler watchfolder.Created, AddressOf logchange
+                watchfolder.EnableRaisingEvents = True
+                btn_startwatch.Enabled = False
+                btn_stop.Enabled = True
+                'initial fire
+                Me.WindowState = FormWindowState.Minimized
+                'Wait for the minimize animation
+                Threading.Thread.Sleep(200)
+                Me.Visible = False
+
+                TrayIcon.ShowBalloonTip(10000)
+
+                Watermark()
+
+
+
+
+            End If
+        End If
+    End Sub
+
+
+    Private Sub APWatermark_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        txt_input.Text = My.Settings.InputPath
+        txt_output.Text = My.Settings.OutputPath
+        If txt_input.Text = "" Then
+            Me.WindowState = FormWindowState.Normal
+        Else
+            CheckBox1.Checked = True
+            btn_startwatch.PerformClick()
+
+        End If
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub logchange(ByVal source As Object, ByVal e As  _
+                        System.IO.FileSystemEventArgs)
+        If e.ChangeType = IO.WatcherChangeTypes.Created Then
+            Watermark()
+        End If
+
+    End Sub
+
+    Private Sub FolderBrowserDialog1_HelpRequest(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FolderBrowserDialog1.HelpRequest
+
+    End Sub
+
+    Sub Watermark()
+        Dim DirInfo As New DirectoryInfo(txt_input.Text & "\")
+        Dim SubDir() As DirectoryInfo = DirInfo.GetDirectories("*")
+        Dim filesPNG() As FileInfo = DirInfo.GetFiles("*.png")
+        For Each File As FileInfo In filesPNG
+            If Not File.Name.Contains("wm_") Then
+
+                Dim wm_File As New FileInfo(File.DirectoryName & "\wm_" & File.Name)
+                If wm_File.Exists() = False Then
+                    Dim p As New ProcessStartInfo
+                    p.FileName = "composite.exe"
+                    p.Arguments = "-quality 100 -resize ""1680>"" -dissolve 30 -gravity southeast -quality 85 AP_Vid_Watermark.png """ & File.FullName.ToString & """ """ & txt_output.Text & "\wm_" & File.Name.ToString & """"
+                    p.WindowStyle = ProcessWindowStyle.Hidden
+                    Dim CompositeProcess As Process = Process.Start(p)
+                    CompositeProcess.WaitForExit()
+                End If
+            End If
+        Next
+
+        Dim filesJPG() As FileInfo = DirInfo.GetFiles("*.jpg")
+        For Each File As FileInfo In filesJPG
+            If Not File.Name.Contains("wm_") Then
+                Dim wm_File As New FileInfo(File.DirectoryName & "\wm_" & File.Name)
+                If wm_File.Exists() = False Then
+                    Dim p As New ProcessStartInfo
+                    p.FileName = "composite.exe"
+                    p.Arguments = "-quality 100 -resize ""1680>"" -dissolve 30 -gravity southeast -quality 85 AP_Vid_Watermark.png """ & File.FullName.ToString & """ """ & txt_output.Text & "\wm_" & File.Name.ToString & """"
+                    p.WindowStyle = ProcessWindowStyle.Hidden
+                    Dim CompositeProcess As Process = Process.Start(p)
+                    CompositeProcess.WaitForExit()
+                End If
+            End If
+        Next
+    End Sub
+
+    Private Sub txt_output_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txt_output.TextChanged
+
+    End Sub
+
+    Private Sub Label4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        FolderBrowserDialog1.ShowDialog()
+        txt_output.Text = FolderBrowserDialog1.SelectedPath
+    End Sub
+
+    Private Sub Button2_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub NotifyIcon1_MouseClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles TrayIcon.MouseClick
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            Me.Visible = True
+            Me.WindowState = FormWindowState.Normal
+        End If
+    End Sub
+
+
+    Private Sub ContextMenuStrip1_Opening(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles ContextMenuStrip1.Opening
+
+    End Sub
+
+    Private Sub ShowToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ShowToolStripMenuItem.Click
+
+        If Me.Visible = True Then
+            Me.Visible = False
+            Me.WindowState = FormWindowState.Minimized
+        Else
+            Me.Visible = True
+            Me.WindowState = FormWindowState.Normal
+        End If
+
+
+    End Sub
+
+    Private Sub ToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            My.Settings.InputPath = txt_input.Text
+            My.Settings.OutputPath = txt_output.Text
+        Else
+            My.Settings.InputPath = ""
+            My.Settings.OutputPath = ""
+        End If
+        My.Settings.Save()
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As System.Object, e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click_2(sender As System.Object, e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Button30_Click(sender As System.Object, e As System.EventArgs)
+
+    End Sub
+
+    Private Sub folderPath_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles folderPath.Click
+        FolderBrowserDialog1.ShowDialog()
+        txt_input.Text = FolderBrowserDialog1.SelectedPath
+        txt_output.Text = FolderBrowserDialog1.SelectedPath
+    End Sub
+
+    Private Sub txt_input_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txt_input.TextChanged
+
+    End Sub
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
